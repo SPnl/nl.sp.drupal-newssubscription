a2370530265f46d2274f29a45a8dafe656d4a9ff
Thomas de Graaff <tdgraaff@sp.nl>
2018-08-31 12:55:29 +0200

    Initial commit

---
+++ js/newssubscription.js
@@ -0,0 +1,26 @@
+(function($, Drupal) {
+  Drupal.ajax.prototype.commands.newssubscription_ga = function(ajax, response, status) {
+    if(window.ga && ga.create) {
+      ga('set', { page: response.path, title: response.title });
+      ga('send', 'pageview');
+    }
+  };
+  Drupal.behaviors.newssubscriptionModule = {
+    attach: function (context, settings) {
+      // Code to be run on page load, and
+      // on ajax load added here
+
+      // Determine scroll 
+      var scrollPos = $('h1').first().offset().top;
+  
+      if(( $( ".messages.error" ).first().length)) {
+        scrollPos = $('.messages.error').first().parent().offset().top;
+      } else {
+        if($( ".newssubscription-navigation" ).length) {
+          scrollPos = $('.newssubscription-navigation').offset().top;
+        }
+      }
+      $('html, body').animate({ scrollTop: scrollPos },300);
+    }
+  };
+})(jQuery, Drupal);
